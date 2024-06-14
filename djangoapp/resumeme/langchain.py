bff7df3013bfeb6c3fd3a019b328244a455c4a80
Pedro Henrique Zaccaria <pedrozaccaria@gmail.com>
2024-06-14 15:31:16 -0300

    add: simple url to markdown displayer

--- djangoapp/resumeme/langchain.py
+++ djangoapp/resumeme/langchain.py
@@ -1,1 +1,25 @@
+# from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
+from langchain.chains.summarize import load_summarize_chain
+from langchain_community.document_loaders import WebBaseLoader, UnstructuredMarkdownLoader
+from langchain_openai import ChatOpenAI
+from langchain_core.documents import Document
+from langchain_community.document_loaders import ToMarkdownLoader
+import markdown
+import os
 
+
+def scrapUrl(valid_url):
+    loader = ToMarkdownLoader(
+        url=valid_url,
+        api_key=os.getenv('MD_API_KEY')
+    )
+    print(loader['error'])
+    print(loader['article'])
+    docs = loader.load()
+
+    assert len(docs) == 1
+    assert isinstance(docs[0], Document)
+    resumed_text = docs[0].page_content
+    html_resumed_text = markdown.markdown(resumed_text)
+
+    return html_resumed_text
--- djangoapp/resumeme/views.py
+++ djangoapp/resumeme/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from resumeme.forms import UrlForm
+from resumeme import langchain
 from django.views import View
 # Create your views here.
 
@@ -7,7 +8,10 @@ from django.views import View
 class Index(View):
     def get(self, request):
         urlform = UrlForm()
+        scrapped_url = request.session.get('scrapped_url', '')
+
         context = {
+            'resumed_content': scrapped_url,
             'form': urlform,
         }
 
@@ -21,14 +25,10 @@ class Index(View):
         form = UrlForm(request.POST)
         if form.is_valid():
             valid_url = form.cleaned_data['to_be_scrapped']
+            print('--------------AAAAAAAAA')
             print(valid_url)
+            resume_response = langchain.scrapUrl(valid_url)
+            request.session['scrapped_url'] = resume_response
 
         form = UrlForm()
-
-        context = {
-            'form': form
-        }
-
-        return redirect(
-            '/',
-        )
+        return redirect('/')
